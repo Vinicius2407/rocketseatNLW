1910d961d966855e646be5705d1a3723c9c86f8f
Vinicius Silva Pereira <viniciussilvapereira24@gmail.com>
2022-02-03 16:16:32 -0300

    Aparecer itens enquanto roda o scroll, refatoração, botão back-to-top

--- main.js
+++ main.js
@@ -34,10 +34,10 @@ for(const link of links) {
 }
 
 /* Mudar o header ao usar scroll */
-const header = document.querySelector("#header");
-const navHeight = header.offsetHeight;
+function changeHeaderWhenScroll() {
+    const header = document.querySelector("#header");
+    const navHeight = header.offsetHeight;
 
-window.addEventListener("scroll", function () {
     if(window.scrollY >= navHeight) {
         //scroll maior que a altura do header
         header.classList.add("scroll");
@@ -45,7 +45,8 @@ window.addEventListener("scroll", function () {
         //menor que a altura do header
         header.classList.remove("scroll");
     }
-});
+
+}
 
 // Testimonials carousel slider Swiper
 const swiper = new Swiper(".swiper", {
@@ -55,4 +56,34 @@ const swiper = new Swiper(".swiper", {
     },
     mousewheel: true,
     // keyboard: true,
+});
+
+// ScrollReveal: Mostrar elementos quando der scroll na página
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .text, #home .image,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links`, 
+{ interval: 100})
+
+// Botão voltar para o topo de
+function backToTop() {
+const backToTopButton = document.querySelector(".back-to-top");
+if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+}else {
+    backToTopButton.classList.remove("show");
+}
+}
+window.addEventListener("scroll", function () {
+    changeHeaderWhenScroll();
+    backToTop();
 });
